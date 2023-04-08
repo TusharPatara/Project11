f1d705acafdde7aa2bb6ac69ec015d96a962cad6
Tushar <tushar.patara11@gmail.com>
2023-04-08 23:40:30 +0530

    first commit

---
+++ Signin.php
@@ -0,0 +1,213 @@
+<?php
+    
+$showAlert = false; 
+$showError = false; 
+$exists=false;
+    
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+      
+    // Include file which makes the
+    // Database Connection.
+    include 'connect.php';   
+    
+    $username = $_POST["username"]; 
+    $password = $_POST["password"]; 
+    $cpassword = $_POST["cpassword"];
+            
+    
+    $sql = "Select * from users where username='$username'";
+    
+    $result = mysqli_query($conn, $sql);
+    
+    $num = mysqli_num_rows($result); 
+    
+    // This sql query is use to check if
+    // the username is already present 
+    // or not in our Database
+    if($num == 0) {
+        if(($password == $cpassword) && $exists==false) {
+    
+            $hash = password_hash($password, 
+                                PASSWORD_DEFAULT);
+                
+            // Password Hashing is used here. 
+            $sql = "INSERT INTO `users` ( `username`,
+                `password`, `date`) VALUES ('$username',
+                '$hash', current_timestamp())";
+    
+            $result = mysqli_query($conn, $sql);
+    
+            if ($result) {
+                $showAlert = true; 
+            }
+        } 
+        else { 
+            $showError = "Passwords do not match"; 
+        }      
+    }// end if 
+    
+   if($num>0) 
+   {
+      $exists="Username not available"; 
+   } 
+    
+}//end if   
+    
+?>
+
+<!-- HTML CODE HERE---- -->
+ 
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <script
+      src="https://kit.fontawesome.com/64d58efce2.js"
+      crossorigin="anonymous"
+    ></script>
+    <link rel="stylesheet" href="style2.css" />
+    <title>Sign in & Sign up Form</title>
+  </head>
+  <body>
+  <?php
+    
+    if($showAlert) {
+    
+        echo ' <div class="alert alert-success 
+            alert-dismissible fade show" role="alert">
+    
+            <strong>Success!</strong> Your account is 
+            now created and you can login. 
+            <button type="button" class="close"
+                data-dismiss="alert" aria-label="Close"> 
+                <span aria-hidden="true">×</span> 
+            </button> 
+        </div> '; 
+    }
+    
+    if($showError) {
+    
+        echo ' <div class="alert alert-danger 
+            alert-dismissible fade show" role="alert"> 
+        <strong>Error!</strong> '. $showError.'
+    
+       <button type="button" class="close" 
+            data-dismiss="alert aria-label="Close">
+            <span aria-hidden="true">×</span> 
+       </button> 
+     </div> '; 
+   }
+        
+    if($exists) {
+        echo ' <div class="alert alert-danger 
+            alert-dismissible fade show" role="alert">
+    
+        <strong>Error!</strong> '. $exists.'
+        <button type="button" class="close" 
+            data-dismiss="alert" aria-label="Close"> 
+            <span aria-hidden="true">×</span> 
+        </button>
+       </div> '; 
+     }
+   
+?>
+    <div class="container">
+      <div class="forms-container">
+        <div class="signin-signup">
+          <form action="#" class="sign-in-form">
+            <h2 class="title">Sign in</h2>
+            <div class="input-field">
+              <i class="fas fa-user"></i>
+              <input type="text" placeholder="Username" />
+            </div>
+            <div class="input-field">
+              <i class="fas fa-lock"></i>
+              <input type="password" placeholder="Password" />
+            </div>
+            <input type="submit" value="Login" class="btn solid" />
+            <p class="social-text">Or Sign in with social platforms</p>
+            <div class="social-media">
+              <a href="#" class="social-icon">
+                <i class="fab fa-facebook-f"></i>
+              </a>
+              <a href="#" class="social-icon">
+                <i class="fab fa-twitter"></i>
+              </a>
+              <a href="#" class="social-icon">
+                <i class="fab fa-google"></i>
+              </a>
+              <a href="#" class="social-icon">
+                <i class="fab fa-linkedin-in"></i>
+              </a>
+            </div>
+          </form>
+          <form action="signin.php" class="sign-up-form" method="post">
+            <h2 class="title">Sign up</h2>
+            <div class="input-field">
+              <i class="fas fa-user" for="username"></i>
+              <input type="text" id="username" placeholder="Username" name="username"/>
+            </div>
+            <div class="input-field">
+              <i class="fas fa-envelope" for="email"></i>
+              <input type="email" id="email" placeholder="Email" name="email"/>
+            </div>
+            <div class="input-field">
+              <i class="fas fa-lock" for="password"></i>
+              <input type="password" id="password" placeholder="Password" name="password"/>
+            </div>
+            <div class="input-field">
+              <i class="fas fa-lock" for="cpassword"></i>
+              <input type="password" placeholder="confirm-password" id="cpassword" name="cpassword"/>
+            </div>
+            <button type="submit" class="btn">Sign up</button>
+            <p class="social-text">Or Sign up with social platforms</p>
+            <div class="social-media">
+              <a href="#" class="social-icon">
+                <i class="fab fa-facebook-f"></i>
+              </a>
+              <a href="#" class="social-icon">
+                <i class="fab fa-twitter"></i>
+              </a>
+              <a href="#" class="social-icon">
+                <i class="fab fa-google"></i>
+              </a>
+              <a href="#" class="social-icon">
+                <i class="fab fa-linkedin-in"></i>
+              </a>
+            </div>
+          </form>
+        </div>
+      </div>
+
+      <div class="panels-container">
+        <div class="panel left-panel">
+          <div class="content">
+            <h3>New here ?</h3>
+            <p>
+              LEARN MORE😀 ! EXPLORE MORE🤩
+            </p>
+            <button class="btn transparent" type="submit" id="sign-up-btn">
+              Sign up
+            </button>
+          </div>
+          <img src="log.svg" class="image" alt="" />
+        </div>
+        <div class="panel right-panel">
+          <div class="content">
+            <h3>One of us ?</h3>
+            <p>
+              LEARN MORE😀 ! EXPLORE MORE🤩
+            </p>
+            <button class="btn transparent" id="sign-in-btn" href="dashoard.html">
+              Sign in
+            </button>
+          </div>
+          <img src="register.svg" class="image" alt="" />
+        </div>
+      </div>
+    </div>
+
+    <script src="app.js"></script>
+  </body>
+</html>
